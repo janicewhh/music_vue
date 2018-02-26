1ee19a0d76030cb4b2091ec245181f3e903d93e5
wanghuihui <wanghuihui@51talk.com>
2018-02-26 10:05:03 +0800

    音乐播放器练习

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png">
+    <router-view/> -->
+    hello world
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style scoped lang="stylus">
+  @import "./common/stylus/variable";
+  #app
+    color: $color-theme
+// #app {
+//   font-family: 'Avenir', Helvetica, Arial, sans-serif;
+//   -webkit-font-smoothing: antialiased;
+//   -moz-osx-font-smoothing: grayscale;
+//   text-align: center;
+//   color: #2c3e50;
+//   margin-top: 60px;
+// }
+</style>
